8521e86560da29038378d1b892441fe910092cdc
Nicholas Ng <nicholasykng@gmail.com>
2019-10-27 17:11:34 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,72 @@
+class TicTacToe
+
+def initialize(board = nil)
+  @board = board || Array.new(9, " ")
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]
+]
+
+def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(index, token = "X")
+  @board[index] = token
+end
+
+def position_taken?(index)
+  if @board[index] == " " || @board[index] == "" || @board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(index)
+  if index.between?(0,8) && !position_taken?(index)
+    return true
+  end
+end
+
+def turn
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  token = current_player
+  if valid_move?(index)
+    move(index, token)
+  else
+    turn
+  end
+  display_board
+end
+
+def turn_count
+  counter = 0
+  @board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player
+  if turn_count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+end
