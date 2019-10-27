f031a89e7ff28561dc57ff2343c4221412434acd
Nicholas Ng <nicholasykng@gmail.com>
2019-10-27 17:37:14 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,129 @@
+class TicTacToe
+
+def initialize(board = nil)
+  @board = board || Array.new(9, " ")
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]
+]
+
+def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(index, token = "X")
+  @board[index] = token
+end
+
+def position_taken?(index)
+  if @board[index] == " " || @board[index] == "" || @board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(index)
+  if index.between?(0,8) && !position_taken?(index)
+    return true
+  end
+end
+
+def turn
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  token = current_player
+  if valid_move?(index)
+    move(index, token)
+  else
+    turn
+  end
+  display_board
+end
+
+def turn_count
+  counter = 0
+  @board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player
+  if turn_count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    end
+  end
+  return false
+end
+
+def full?
+  @board.all? {|i| i == "X" || i == "O"}
+end
+
+def draw?
+  if !won? && full?
+    return true
+  else
+    return false
+  end
+end
+
+def over?
+  if won?
+    return true
+  elsif draw?
+    return true
+  elsif full?
+    return true
+  end
+end
+
+def winner
+  if won?
+    return @board[won?[0]]
+  end
+end
+
+def play
+  until over?
+    turn
+  end
+  if won?
+    winner == "X" || winner == "O"
+    puts "Congratulations #{winner}!"
+  else draw?
+    puts "Cat's Game!"
+  end
+end
+
+end
